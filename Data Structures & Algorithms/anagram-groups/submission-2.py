97e82c369fe073c74e495497970e1a3213e3d008
Ashwik Raj <f20230313@goa.bits-pilani.ac.in>
2026-09-04 16:39:29 +0530

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        seen = {}
+        for word in strs:
+            signature = "".join(sorted(word))
+            if signature in seen:
+                seen[signature].append(word)
+            else:
+                seen[signature] = [word]
+        return list(seen.values())
+
+    # def DNA(self, str1: str):
+    #     hash = []
+    #     alphabet = "abcdefghijklmnopqrstuvwxyz"
+    #     for char in alphabet:
+    #         hash.append(str1.count(char))
+    #     return tuple(hash)
